08603b30b69bfc0b6bafd43011de99a9f09ac77d
dogrod <rodrickjue@hotmail.com>
2018-08-23 17:34:49 +0800

    :sparkles: Basic stylesheet & event handler

---
+++ assets/js/index.js
@@ -0,0 +1,17 @@
+const sites = {
+  blog: 'http://blog.dogrod.com',
+  fmf: 'http://fmf.buzz',
+  github: 'https://github.com/dogrod',
+}
+
+const handleClickPlate = (e) => {
+  const target = e.target.getAttribute('data-target')
+
+  const url = sites[target]
+
+  window.location.href = url
+}
+
+const plates = document.querySelectorAll('.plate')
+
+plates.forEach(plate => plate.addEventListener('click', handleClickPlate))
